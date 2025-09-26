724c45db47a00bc5772764a7b40727be3c73c9df
JSwong41 <jaisvenwong@gmail.com>
2025-09-26 16:59:02 +0800

    Update demo.py

--- spiders/demo.py
+++ spiders/demo.py
@@ -2,7 +2,6 @@ import json
 import requests
 from com.github.catvod import Proxy
 from abc import ABCMeta, abstractmethod
-from lxml import etree
 
 class Spider(metaclass=ABCMeta):
     _instance = None
@@ -38,35 +37,82 @@ class Spider(metaclass=ABCMeta):
 
 class DemoSpider(Spider):
     def init(self, extend=""):
-        # 指向你在 GitHub 上的 sites.json Raw URL
         self.sites_url = "https://raw.githubusercontent.com/JSwong41/tvbox-spiders/refs/heads/main/assets/sites.json"
         self.sites = json.loads(self.fetch(self.sites_url))['sites']
 
+    # 首页分类，支持二级分类
     def homeContent(self, filter=None):
         classes = []
         for site in self.sites:
-            classes.append({"type_id": site['key'], "type_name": site['name']})
+            # 二级分类：类型 + 年份
+            categories = [
+                {
+                    "type_id": f"{site['key']}_type",
+                    "type_name": "类型",
+                    "filters": [
+                        {"n": "动作片", "v": "动作片"},
+                        {"n": "喜剧片", "v": "喜剧片"},
+                        {"n": "爱情片", "v": "爱情片"},
+                        {"n": "科幻片", "v": "科幻片"},
+                        {"n": "恐怖片", "v": "恐怖片"},
+                        {"n": "剧情片", "v": "剧情片"},
+                        {"n": "纪录片", "v": "纪录片"},
+                        {"n": "动画片", "v": "动画片"}
+                    ]
+                },
+                {
+                    "type_id": f"{site['key']}_year",
+                    "type_name": "年份",
+                    "filters": [{"n": str(y), "v": str(y)} for y in range(2025, 2015, -1)]
+                }
+            ]
+            classes.append({"type_id": site['key'], "type_name": site['name'], "class": categories})
         return {"class": classes}
 
+    # 分类内容
     def categoryContent(self, tid, pg, filter, extend):
-        site = next((s for s in self.sites if s['key'] == tid), None)
-        if not site:
-            return {"list": []}
-        url = site['api']
-        try:
-            data = json.loads(self.fetch(url))
-            vods = []
-            for item in data.get("list", []):
-                vods.append({
-                    "vod_id": f"{tid}${item['vod_id']}",
-                    "vod_name": item.get("vod_name", ""),
-                    "vod_pic": item.get("vod_pic", ""),
-                    "vod_remarks": item.get("vod_remarks", "")
-                })
-            return {"list": vods}
-        except:
-            return {"list": []}
+        # 区分二级分类类型
+        if "_type" in tid or "_year" in tid:
+            site_key = tid.split("_")[0]
+            site = next((s for s in self.sites if s['key'] == site_key), None)
+            if not site:
+                return {"list": []}
+            params = {}
+            if filter:
+                for f in filter:
+                    params[f['key']] = f['value']
+            try:
+                data = json.loads(self.fetch(site['api'], params=params))
+                vods = []
+                for item in data.get("list", []):
+                    vods.append({
+                        "vod_id": f"{site['key']}${item['vod_id']}",
+                        "vod_name": item.get("vod_name", ""),
+                        "vod_pic": item.get("vod_pic", ""),
+                        "vod_remarks": item.get("vod_remarks", "")
+                    })
+                return {"list": vods}
+            except:
+                return {"list": []}
+        else:
+            site = next((s for s in self.sites if s['key'] == tid), None)
+            if not site:
+                return {"list": []}
+            try:
+                data = json.loads(self.fetch(site['api']))
+                vods = []
+                for item in data.get("list", []):
+                    vods.append({
+                        "vod_id": f"{tid}${item['vod_id']}",
+                        "vod_name": item.get("vod_name", ""),
+                        "vod_pic": item.get("vod_pic", ""),
+                        "vod_remarks": item.get("vod_remarks", "")
+                    })
+                return {"list": vods}
+            except:
+                return {"list": []}
 
+    # 详情内容
     def detailContent(self, ids):
         tid, vid = ids.split('$')
         site = next((s for s in self.sites if s['key'] == tid), None)
@@ -93,6 +139,7 @@ class DemoSpider(Spider):
         except:
             return {}
 
+    # 搜索
     def searchContent(self, key, quick, pg="1"):
         results = []
         for site in self.sites:
@@ -110,6 +157,7 @@ class DemoSpider(Spider):
                 continue
         return {"list": results}
 
+    # 播放
     def playerContent(self, flag, id, vipFlags):
         tid, vid = id.split('$')
         site = next((s for s in self.sites if s['key'] == tid), None)
